1ca3ebf3fd4882c45570db2a0977c69133c9eab0
Ezer Neupane Rames <ezerames@gmail.com>
2016-03-05 16:03:54 +0545

    kalman update

--- kalman.py
+++ kalman.py
@@ -35,13 +35,15 @@ for k in range(1,n_iter):
     xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
     P[k] = (1-K[k])*Pminus[k]
 
+estimate = int(xhat[-1])
+
 pylab.figure()
 pylab.plot(z,'k+',label='Feeded Data')
 pylab.plot(xhat,'b-',label='Estimatation')
 
+#pylab.plot(estimate,'',label='Doctors Required')
 pylab.legend()
 pylab.xlabel('Year')
 pylab.ylabel('No. of Doctors Required')
-
+pylab.title('No. of Doctors Required for year 2072 is : '+str(estimate))
 pylab.show()
-print(xhat[-1])
